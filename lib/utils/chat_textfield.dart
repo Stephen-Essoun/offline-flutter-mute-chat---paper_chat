714b7ff7a891d99ea53766447d844979979e6827
Stephen Essoun <boit7845@gmail.com>
2023-02-28 20:28:15 +0000

    done

--- lib/utils/chat_textfield.dart
+++ lib/utils/chat_textfield.dart
@@ -6,6 +6,7 @@ import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../module/chat_module.dart';
+import 'colors.dart';
 
 class ChatTextField extends StatefulWidget {
   const ChatTextField({
@@ -33,83 +34,80 @@ class _ChatTextFieldState extends State<ChatTextField> {
         builder: (context, val, _) {
           return Container(
             padding: const EdgeInsets.all(8.0),
-            // color: Colors.black12,
-            child:
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                //   children: [
-                // const SizedBox(
-                //   width: 10,
-                // ),
+            color: bgColor,
+            child: Flex(
+              direction: Axis.horizontal,
+              children: [
                 Expanded(
-              child: TextFormField(
-                controller: _controller,
-                maxLines: 3,
-                minLines: 1,
-                decoration: InputDecoration(
-                  hintText: 'Type your message here',
-                  prefixIcon: GestureDetector(
-                    onTap: val.text.isNotEmpty
-                        ? () async {
-                            var message = ChatMessage(
-                              messageContent: _controller.text,
-                              time: DateTime.now(),
-                              messageType: 'receiver',
-                            );
-                            provider.addMessage(message);
-                            _controller.clear();
-                          }
-                        : null,
-                    child: Container(
-                      padding: const EdgeInsets.all(5),
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        // color: Color.fromARGB(255, 15, 197, 179),
+                  child: TextFormField(
+                    controller: _controller,
+                    maxLines: 3,
+                    minLines: 1,
+                    cursorColor: mainColor,
+                    decoration: InputDecoration(
+                      hintText: 'Type your message here',
+                      prefixIcon: GestureDetector(
+                        onTap: val.text.isNotEmpty
+                            ? () async {
+                                var message = ChatMessage(
+                                  messageContent: _controller.text,
+                                  time: DateTime.now(),
+                                  messageType: 'receiver',
+                                );
+                                provider.addMessage(message);
+                                _controller.clear();
+                              }
+                            : null,
+                        child: Container(
+                          padding: const EdgeInsets.all(5),
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            // color: Color.fromARGB(255, 15, 197, 179),
+                          ),
+                          child: Icon(
+                            Icons.send,
+                            textDirection: TextDirection.rtl,
+                            color: val.text.isNotEmpty ? mainColor : grey,
+                          ),
+                        ),
                       ),
-                      child: Icon(
-                        Icons.send,
-                        textDirection: TextDirection.rtl,
-                        color: val.text.isNotEmpty ? Colors.white : Colors.grey,
+                      suffixIcon: GestureDetector(
+                        onTap: val.text.isNotEmpty
+                            ? () {
+                                var message = ChatMessage(
+                                  messageContent: _controller.text,
+                                  time: DateTime.now(),
+                                  messageType: 'sender',
+                                );
+                                provider.addMessage(message);
+                                _controller.clear();
+                              }
+                            : null,
+                        child: Container(
+                          padding: const EdgeInsets.all(5),
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            // color: Color.fromARGB(255, 15, 197, 179),
+                          ),
+                          child: Icon(
+                            Icons.send,
+                            color: val.text.isNotEmpty ? mainColor : grey,
+                          ),
+                        ),
                       ),
+                      isDense: true,
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 5.0, horizontal: 15.0),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(25)),
+                      filled: true,
+                      fillColor: white,
+                      hintStyle: const TextStyle(color: Colors.grey),
                     ),
                   ),
-                  suffixIcon: GestureDetector(
-                    onTap: val.text.isNotEmpty
-                        ? () {
-                            var message = ChatMessage(
-                              messageContent: _controller.text,
-                              time: DateTime.now(),
-                              messageType: 'sender',
-                            );
-                            provider.addMessage(message);
-                            _controller.clear();
-                          }
-                        : null,
-                    child: Container(
-                      padding: const EdgeInsets.all(5),
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        // color: Color.fromARGB(255, 15, 197, 179),
-                      ),
-                      child: Icon(
-                        Icons.send,
-                        color: val.text.isNotEmpty
-                            ? Colors.blueAccent
-                            : Colors.grey,
-                      ),
-                    ),
-                  ),
-                  isDense: true,
-                  contentPadding: const EdgeInsets.symmetric(
-                      vertical: 5.0, horizontal: 15.0),
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(25)),
-                  filled: true,
-                  fillColor: Colors.lightBlueAccent,
-                  labelStyle: const TextStyle(color: Colors.grey),
                 ),
-              ),
+              ],
             ),
             // const SizedBox(
             //   width: 10,
---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const mainColor = Color(0xffff00ff);
+const white = Color(0xffffffff);
+const bgColor = Color.fromARGB(255, 199, 199, 194);
+const color = Color.fromARGB(255, 252, 169, 252);
+const grey = Colors.grey;
